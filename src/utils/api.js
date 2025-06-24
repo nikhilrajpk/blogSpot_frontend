229498df7ae95102492d3a39e54e05ce44545b63
nikhilrajpk <nikhilrajpk2002@gmail.com>
2025-06-24 14:17:24 +0530

    Final commit : Deployment version baseUrl updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cms.nikhilrajpk.in/api',
+  baseURL: 'https://blogspot-backend.nikhilrajpk.in/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
